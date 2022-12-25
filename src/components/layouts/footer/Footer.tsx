da13c259b37f171a3b00aa280b3fa2c6682ccf07
takagi <takagi09245@gmail.com>
2022-12-25 15:39:31 +0900

    フッター作成

---
+++ src/components/layouts/footer/Footer.tsx
@@ -0,0 +1,19 @@
+import { Container } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Container
+      bg="white"
+      maxW="100%"
+      fontSize="sm"
+      textAlign="center"
+      borderTop="1px"
+      borderColor="gray.200"
+      p={2}
+    >
+      &copy; 2022 RamenSharing All rights reserved.
+    </Container>
+  );
+};
+
+export default Footer;
--- src/components/layouts/layouts.tsx
+++ src/components/layouts/layouts.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import { ReactNode } from "react";
+import Footer from "./footer/Footer";
 import Header from "./header/Header";
 
 type Props = { children: ReactNode };
@@ -8,9 +9,10 @@ const Layout = ({ children }: Props) => {
   return (
     <>
       <Header />
-      <Box h="100vh" w="100%" bg="orange.50" pt={8} pb={8} mt={20} mb={30}>
+      <Box h="100vh" w="100%" bg="orange.50" pt={8} mt={20}>
         {children}
       </Box>
+      <Footer />
     </>
   );
 };
